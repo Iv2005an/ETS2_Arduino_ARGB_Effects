065f39c96a2e7fc2fd25ecb5e7bdb6073c560198
Ivan <telitsinivan05@yandex.ru>
2023-03-21 02:57:42 +0300

    c

--- Arduino_ARGB_Effects_Sketch/Arduino_ARGB_Effects_Sketch.ino
+++ Arduino_ARGB_Effects_Sketch/Arduino_ARGB_Effects_Sketch.ino
@@ -1,8 +1,113 @@
+#include "FastLED.h"
+
+#define LED_PIN 2    // пин ленты
+#define NUMLEDS 102  // количество светодиодов
+
+CRGB argb[NUMLEDS];
+byte brightness = 100;
+void l_blinler() {
+  for (int i = NUMLEDS; i >= 0; i--) {
+    argb[i] = CRGB::OrangeRed;
+    FastLED.show();
+    delay(5);
+  }
+  for (int i = NUMLEDS; i >= 0; i--) {
+    argb[i] = CRGB::Black;
+    FastLED.show();
+    delay(5);
+  }
+  FastLED.clear();
+  // Serial.println("l_blinler");
+}
+
+void r_blinler() {
+  for (int i = 0; i < NUMLEDS; i++) {
+    argb[i] = CRGB::OrangeRed;
+    FastLED.show();
+    delay(5);
+  }
+  for (int i = 0; i < NUMLEDS; i++) {
+    argb[i] = CRGB::Black;
+    FastLED.show();
+    delay(5);
+  }
+  FastLED.clear();
+  // Serial.println("r_blinler");
+}
+
+void hazard() {
+  int center = 0;
+  if (NUMLEDS % 2 == 0) center = NUMLEDS / 2 - 1;
+  else int center = NUMLEDS / 2;
+  for (int i_l = center, i_r = center + 1; i_l >= 0 && i_r < NUMLEDS; i_l--, i_r++) {
+    argb[i_l] = CRGB::OrangeRed;
+    argb[i_r] = CRGB::OrangeRed;
+    FastLED.show();
+    delay(10);
+  }
+  for (int i_l = center, i_r = center + 1; i_l >= 0 && i_r < NUMLEDS; i_l--, i_r++) {
+    argb[i_l] = CRGB::Black;
+    argb[i_r] = CRGB::Black;
+    FastLED.show();
+    delay(10);
+  }
+  FastLED.clear();
+  // Serial.println("r_blinler");
+}
+
+void warning() {
+  for (int i = 0; i <= brightness; i++) {
+    FastLED.setBrightness(i);
+    FastLED.showColor(CRGB::Red);
+    delay(10);
+  }
+  for (int i = NUMLEDS; i >= 0; i--) {
+    FastLED.setBrightness(i);
+    FastLED.showColor(CRGB::Red);
+    delay(10);
+  }
+  FastLED.setBrightness(brightness);
+  FastLED.clear();
+}
+
 void setup() {
-  Serial.begin(9600);
+  FastLED.addLeds< WS2812, LED_PIN, GRB>(argb, NUMLEDS);
+  FastLED.clear();
+  FastLED.show();
+  FastLED.setBrightness(brightness);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 1500);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (Serial.available() > 0) {
+    char input[7] = { 0 };
+    for (int i = 0; i < 7; i++) {
+      input[i] = (char)Serial.read();
+      delay(1);
+    }
+    // for (int i = 0; i < 7; i++) {
+    //   Serial.print(input[i]);
+    // }
+    if (input[0] == '1') {
+      for (int i = 0; i < 7; i++) input[i] = NULL;
+      hazard();
+    } else if (input[2] == '1') {
+      for (int i = 0; i < 7; i++) input[i] = NULL;
+      l_blinler();
+    } else if (input[4] == '1') {
+      for (int i = 0; i < 7; i++) input[i] = NULL;
+      r_blinler();
+    } else if (input[6] == '1') {
+      for (int i = 0; i < 7; i++) input[i] = NULL;
+      warning();
+    }
+    // for (int i = 0; i < 7; i++) {
+    //   Serial.print(input[i]);
+    // }
+  }
 }
+// l_blinler();
+// r_blinler();
+// hazard();
+// warning();
--- C#_Reader/Program.cs
+++ C#_Reader/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System.IO.MemoryMappedFiles;
 using System.Runtime.InteropServices;
-
+using System.IO.Ports;
+bool error;
+SerialPort port;
+do
+{
+    error = false;
+    port = new SerialPort(GetArduinoPort(), 115200);
+    port.Encoding = System.Text.Encoding.UTF8;
+    try
+    {
+        port.Open();
+    }
+    catch (UnauthorizedAccessException)
+    {
+        error = true;
+        Console.WriteLine("\nПорт уже открыт!!!");
+    }
+} while (error);
 telemetry_state_t telemetry = new telemetry_state_t();
 while (true)
 {
     try
     {
+#pragma warning disable CA1416 // Проверка совместимости платформы
         using (MemoryMappedFile sharedMemory = MemoryMappedFile.OpenExisting("ARGB_Telemetry_Data"))
         {
             using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor())
@@ -13,16 +31,53 @@ while (true)
                 reader.Read(0, out telemetry);
             }
         }
+#pragma warning restore CA1416 // Проверка совместимости платформы
     }
     catch { continue; }
-    Console.WriteLine($"Скорость(м/с): {telemetry.speed};\nЛевый поворотник: {telemetry.l_blinker};\n" +
-        $"Правый поворотник: {telemetry.r_blinker};\nАварийка: {telemetry.hazard};\n" +
-        $"Ручник: {telemetry.parking_brake};\nНехватка воздуха: {telemetry.low_air};\n" +
-        $"Нехватка топлива: {telemetry.low_fuel}");
-    System.Threading.Thread.Sleep(50);
+    //Console.WriteLine($"Скорость(м/с): {telemetry.speed};\nЛевый поворотник: {telemetry.l_blinker};\n" +
+    //    $"Правый поворотник: {telemetry.r_blinker};\nАварийка: {telemetry.hazard};\n" +
+    //    $"Ручник: {telemetry.parking_brake};\nНехватка воздуха: {telemetry.low_air};\n" +
+    //    $"Нехватка топлива: {telemetry.low_fuel}");
     Console.Clear();
-}
+    string to_send = $"{(telemetry.hazard ? 1 : 0)} {(telemetry.l_blinker ? 1 : 0)} {(telemetry.r_blinker ? 1 : 0)} {(telemetry.parking_brake || telemetry.low_air || telemetry.low_fuel ? 1 : 0)}";
+    Console.WriteLine(to_send);
+    port.Write(to_send);
+    Thread.Sleep(3000);
 
+}
+string GetArduinoPort()
+{
+    string[] ports = SerialPort.GetPortNames();
+    int i = 1;
+    foreach (var port in ports)
+    {
+        Console.WriteLine(i++ + " " + port);
+    }
+    bool error;
+    int portIndex = 1;
+    do
+    {
+        error = false;
+        Console.Write("Введите индекс порта для чтения: ");
+        try
+        {
+            portIndex = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            error = true;
+        }
+        if (portIndex > i - 1 || portIndex <= 0)
+        {
+            error = true;
+        }
+        if (error)
+        {
+            Console.WriteLine("Ошибка!!!");
+        }
+    } while (error);
+    return ports[portIndex - 1];
+}
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 struct telemetry_state_t
 {
