acf4721cb28a94648ca86f3e2bc93863c56514fd
Ivan <telitsinivan05@yandex.ru>
2023-04-03 00:13:27 +0300

    .

--- ETS2_ARGB_Settings/MainWindow.xaml.cs
+++ ETS2_ARGB_Settings/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace ETS2_ARGB_Settings_Aplication
 {
@@ -14,42 +15,47 @@ namespace ETS2_ARGB_Settings_Aplication
         {
             foreach (string arg in Environment.GetCommandLineArgs())
             {
-                if (arg == "start_from_ets2") SendKeys.SendWait("%{Tab}");
+                if (arg == "start_from_ets2") { SendKeys.SendWait("%{Tab}"); break; }
             }
             InitializeComponent();
+            COM_ComboBox.ItemsSource = SerialPort.GetPortNames();
+            COM_ComboBox.SelectedIndex = -1;
         }
 
 
         // Чекбокс вкл/выкл
         private void Enable_CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            COM_ComboBox.IsEnabled = true;
+            COM_ComboBox.SelectedIndex = 0;
         }
         private void Enable_CheckBox_Unhecked(object sender, RoutedEventArgs e)
         {
-
+            COM_ComboBox.IsEnabled = false;
+            COM_ComboBox.SelectedIndex = -1;
         }
 
 
         // Список с портами
         private void COM_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            COM_ComboBox.ItemsSource = SerialPort.GetPortNames();
         }
         private void COM_ComboBox_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-
+            COM_ComboBox.ItemsSource = SerialPort.GetPortNames();
         }
         private void COM_ComboBox_DropDownOpened(object sender, EventArgs e)
         {
-
+            COM_ComboBox.ItemsSource = SerialPort.GetPortNames();
         }
         private void COM_ComboBox_DropDownClosed(object sender, EventArgs e)
         {
-
+            COM_ComboBox.ItemsSource = SerialPort.GetPortNames();
         }
 
 
+        // Список с эффектами
         private void Effects_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
